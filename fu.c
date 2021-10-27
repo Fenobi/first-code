03c409d7e0b13ba9382dde43dbf3349eb6f9f984
Kenobi <Kenobi@KENOBI>
2021-10-27 16:20:13 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", ~a);
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a', 'b', 'c'};
+//	printf("%d\n", strlen(arr1));
+//	printf("%d\n", strlen(arr2));
+//	return 0;
+//}
+//int main()
+//{
+//	int i, sum = 0;
+//	for (i = 1;i <= 100;i++)
+//	{
+//		sum = i + sum;
+//	}
+//	printf("%d\n",sum);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("给李嘉慧同学写的代码");
+//	return 0;
+//}
+
+int main()
